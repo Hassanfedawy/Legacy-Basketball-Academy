9d4bb02ac9fef1ecbc6ecc545b9b2a881da5f801
Alhassan <alhassanfedawy620@gmail.com>
2024-09-23 22:44:34 +0300

    .

--- app/Components/HeroSection.tsx
+++ app/Components/HeroSection.tsx
@@ -1,7 +1,4 @@
-"use client";
 import Link from "next/link";
-import { motion } from "framer-motion";
-
 function HeroSection() {
   const fadeIn = {
     hidden: { opacity: 0, y: 20 },
@@ -13,7 +10,7 @@ function HeroSection() {
       {/* Background Video */}
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src="/Videos/IMG_5052.MOV"
+        src="/Videos/img 5052.mp4"
         autoPlay
         loop
         muted
@@ -24,46 +21,34 @@ function HeroSection() {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4 text-white">
-        <motion.h1
+        <h1
           className="text-4xl md:text-6xl font-bold"
-          initial="hidden"
-          animate="visible"
-          variants={fadeIn}
-          transition={{ duration: 0.6 }}
+          
+          
         >
           Train Like a Champion
-        </motion.h1>
+        </h1>
 
-        <motion.p
+        <p
           className="mt-4 text-lg md:text-xl text-secondary-text"
-          initial="hidden"
-          animate="visible"
-          variants={fadeIn}
-          transition={{ duration: 0.6, delay: 0.2 }}
+        
         >
           Achieve your fitness goals and dominate the court!
-        </motion.p>
+        </p>
 
-        <motion.div
-          className="mt-8"
-          initial="hidden"
-          animate="visible"
-          variants={fadeIn}
-          transition={{ duration: 0.6, delay: 0.4 }}
-        >
+        <div  className="mt-8">
           <Link
             href="#Home"
             className="bg-button-bg text-black py-3 px-6 rounded-md shadow-md hover:bg-button-hover transition duration-300"
-          >
-            Join Now
-          </Link>
+  >  Join Now</Link>
+  
           <Link
             href="#Programs"
             className="ml-4 bg-transparent border border-white py-3 px-6 rounded-md hover:bg-white hover:text-black transition duration-300"
           >
             Learn More
           </Link>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
